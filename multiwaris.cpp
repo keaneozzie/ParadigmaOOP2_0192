3e953e855a97309208ac5bc903ad3a7b4190f8b2
keaneozzie <ozziedharma897@gmail.com>
2026-05-18 15:10:23 +0700

    menambah class pekerja dan pelajar

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -11,3 +11,21 @@ public:
     }
 };
 
+class pekerja : virtual public orang {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
