f50dbcdd6a8119ddcf071ccaca5ab7c83fa0758d
keaneozzie <ozziedharma897@gmail.com>
2026-05-18 16:01:32 +0700

    menambahkan class pekerja dan pelajar inheritance dari orang

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -12,3 +12,21 @@ public:
     }
 };
 
+class pekerja : public orang {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
