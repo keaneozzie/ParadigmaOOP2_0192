b3ce94e50c52c1db6d6fed9c5f082d246ba3fb89
keaneozzie <ozziedharma897@gmail.com>
2026-05-18 15:05:32 +0700

    memasukkan nama data lampu rumah

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -15,3 +15,12 @@ public:
     }
 };
 
+int main() {
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+   
